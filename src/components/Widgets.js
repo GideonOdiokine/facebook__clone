c82cc4d9e9d235ca4f9f54fe36d1a63dbc5181f6
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2021-07-27 05:07:45 +0100

    Update Widgets.js

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -5,6 +5,7 @@ function Widgets() {
   return (
     <div className="widgets">
       <iframe
+    title="facebook frame"
         src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fgidysmart.odiokineh.3%2Fposts%2F939037390192464&show_text=true&width=340"
         width="340"
         height="1500"
